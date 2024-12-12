d8682da099ece858e878ff8f92f9ccd0bc523fac
betalantz <betalantz@gmail.com>
2024-12-12 08:22:03 -0800

    end code 03

--- 03_oop/debug.py
+++ 03_oop/debug.py
@@ -2,5 +2,16 @@
 
 from lib.pet import *
 
+chase = Pet("Chase", 7, "pug", "rowdy", "./assets/cookie.jpg")
+brooke = Pet("Brooke", 8, "American shorthair", "chill", "./assets/rose.jpg")
 
-import ipdb; ipdb.set_trace()
+chase.age = 8
+print(chase.age)
+
+brooke.birthday()
+print(brooke)
+
+
+import ipdb
+
+ipdb.set_trace()
